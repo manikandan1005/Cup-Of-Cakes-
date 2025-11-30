915622498c4d6960f3611ef504f03b3f17ae121d
Manikandan S <your-ceitmanikandan24@gmail.com>
2025-11-30 17:34:12 +0530

    ui update in productlist

---
+++ src/Components/ProductList/Card.jsx
@@ -0,0 +1,24 @@
+import './Card.scss'
+
+
+function Card(props){
+    return (
+
+                           <div className="col-lg-3 col-md-4 col-6 div">
+                        <div className="card my-3" style={{width:"18rem"}}>
+                            <img src={props.image} className="card-img-top" alt="..." />
+                              <div className="card-body">
+                                 <h5 className="card-title">{props.name}</h5>
+                                 <p className="card-text">{props.description}</p>
+                                 <p className="card-text">{props.price}</p>
+                                 
+                                 <a href="#" className="btn btn-primary">Buy</a>
+                            </div>
+                        </div>
+                   </div>
+
+
+    );
+}
+
+export default Card;
